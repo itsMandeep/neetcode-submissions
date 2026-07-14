0d5aadaed5383d5f929397066e9de8ed3592c204
Mandeep Singh <smandeepsinghk@gmail.com>
2026-07-14 10:57:08 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.js
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+
+    //input = array {ints}
+    //output boolean
+
+    // brute 
+    // i = 0 - length of n
+    // j = i+1 - length of n
+    // if i == j return true
+    // log o(n^2) o(1)
+
+    // optimised solution
+    // object for lookup
+    // loop from 0 - n
+    // if object contains value as key - return true
+    // save that value as key and assign true
+    // log o(n) | o(n)
+    hasDuplicate(nums) {
+        // const length = nums.length
+        // const numsObj = {}
+
+        // for(let i =0; i<length; i++){
+        //     if(Object.hasOwn(numsObj, nums[i])){
+        //         return true;
+        //     }
+
+        //     numsObj[nums[i]] = true
+        // }
+
+        // return false
+
+        const visitedNumbers = new Set()
+
+        for(let value of nums){
+            if(visitedNumbers.has(value)){
+                return true
+            }
+
+            visitedNumbers.add(value)
+        }
+
+        return false
+    }
+}
