d0c9b316883dd454578bbed43888896ba53614f3
Mandeep Singh <smandeepsinghk@gmail.com>
2026-07-17 19:36:15 +0530

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.ts
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+
+    // // NAIVE - n log n
+    // topKFrequent(nums: number[], k: number): number[] {
+
+    //     if(nums.length === 0) []
+    //     if(nums.length === 1) [nums]
+
+    //     const map = new Map<number, number>()
+
+    //     for(let num of nums){
+    //         map.set(num, (map.get(num) ?? 0) + 1)
+    //     }
+
+    //     return [...map.entries()].sort((a,b) => b[1] - a[1]).splice(0, k).map((value) => value[0])
+    // }
+
+    // NAIVE - n log n
+    topKFrequent(nums: number[], k: number): number[] {
+        if (nums.length === 0) return [];
+        if (nums.length === 1) return nums;
+
+        const buckets = Array.from({ length: nums.length + 1 }, () => []);
+
+        const map = new Map<number, number>();
+
+        for (let num of nums) {
+            map.set(num, (map.get(num) ?? 0) + 1);
+        }
+
+        for (let [key, value] of map) {
+            (buckets[value] ?? []).push(key);
+        }
+
+        return buckets.flat().splice(-k, k);
+    }
+}
